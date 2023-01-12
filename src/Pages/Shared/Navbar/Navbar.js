6cdd2a07c985f1dab85a41a7b1429daff0f9f6e8
Rakib Linux <rakibulinux@gmail.com>
2023-01-12 15:29:38 +0600

    profile picture added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
       </li>
 
       <li>
-        <NavLink>Sign In</NavLink>
+        <NavLink to="/login">Sign In</NavLink>
       </li>
     </>
   );
@@ -162,27 +162,27 @@ const Navbar = () => {
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img
-                    src="https://placeimg.com/80/80/people"
-                    alt="profile pictures"
-                  />
+                  <img src={user?.photoURL} alt="profile pictures" />
                 </div>
               </label>
               <ul
                 tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 "
               >
-                <li>
-                  <NavLink className="justify-between">
+                <li className="">
+                  <NavLink className="justify-between bg-white text-black">
                     Profile
-                    <span className="badge">New</span>
                   </NavLink>
                 </li>
-                <li>
-                  <NavLink>Settings</NavLink>
+                <li className="">
+                  <NavLink className="justify-between bg-white text-black">
+                    Settings
+                  </NavLink>
                 </li>
-                <li>
-                  <NavLink>Logout</NavLink>
+                <li className="">
+                  <NavLink className="justify-between bg-white text-black">
+                    Logout
+                  </NavLink>
                 </li>
               </ul>
             </div>

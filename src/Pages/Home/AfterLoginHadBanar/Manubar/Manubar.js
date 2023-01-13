358f138ae5d6c693e0def77894bf0cac8ad40ad6
unknown <mahamuda3052@gmail.com>
2023-01-13 11:37:53 +0600

    create two deffrent home page

---
+++ src/Pages/Home/AfterLoginHadBanar/Manubar/Manubar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Manubar = () => {
+    return (
+        <div style={{width:"100%"}}>
+            <ul className="menu menu-vertical lg:menu-horizontal bg-base-100 border my-8" style={{width:"100%",alignItems:'center'}}>
+  <li><Link to='/'>programing</Link></li>
+  <li><Link to='/'>React.js</Link></li>
+  <li><Link to='/'>Node.js</Link></li>
+  <li><Link to='/'>Traveling</Link></li>
+  <li><Link to='/'>Javascript</Link></li>
+  <li><Link to='/'>UX Design</Link></li>
+  
+
+</ul>
+        </div>
+    );
+};
+
+export default Manubar;

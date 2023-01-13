874d4fafba1896b5941fc2b005b0aec0adaabbaf
unknown <mahamuda3052@gmail.com>
2023-01-13 12:11:06 +0600

    modify some code

--- src/Pages/Home/AfterLoginHadBanar/AfterLoginHadBanar.js
+++ src/Pages/Home/AfterLoginHadBanar/AfterLoginHadBanar.js
@@ -11,7 +11,7 @@ const AfterLoginHadBanar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
       </button>
     </div>
-    <p>Get unlimited access to all of FreeMium for less than $1/week.</p>
+    <Link to='/'>Get unlimited access to all of FreeMium for less than $1/week.</Link>
    <Link to='/' className='text-[red]'>Become a member</Link>
   
   </div>

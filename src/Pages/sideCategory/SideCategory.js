a55f37d6c85c4916db8c820c8f7deeeb24441795
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2023-01-13 11:24:43 +0600

    RH side category add

--- src/Pages/sideCategory/SideCategory.js
+++ src/Pages/sideCategory/SideCategory.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import SideCategoryButton from './sideCategoryButton/SideCategoryButton';
 
 const SideCategory = () => {
+    const [categoryButtons, setCategoryButtons] = useState([]);
+    console.log(categoryButtons);
+    useEffect(()=>{
+        fetch('http://localhost:5000/categoryButton')
+        .then(res=>res.json())
+        .then(data=>setCategoryButtons(data))
+    },[])
     return (
-        <div className='mt-5'>
-            SideCategory
+        <div className='mt-5 p-4 sticky top-0'>
+            <p className='text-base font-semibold text-gray-800'>DISCOVER MORE OF WHAT MATTERS TO YOU</p>
+            <div className="grid grid-cols-3 gap-2 mt-4">
+            {
+                categoryButtons.map(data=>
+                    <SideCategoryButton data={data}></SideCategoryButton>
+                    )
+            }
+            </div>
+           
         </div>
     );
 };

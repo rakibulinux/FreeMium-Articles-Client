fc42a2f5067b071294e7434d023add69c0b7ec43
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2023-01-15 22:40:18 +0600

    fix responsivess

--- src/components/particleJS/Particle.js
+++ src/components/particleJS/Particle.js
@@ -2,20 +2,10 @@ import { useCallback } from "react";
 import { Particles } from "react-tsparticles";
 import { loadFull } from "tsparticles";
 export default function Particle() {
-<<<<<<< HEAD
-    const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        await loadFull(engine);
-    }, []);
-=======
   const particlesInit = useCallback(async (engine) => {
     console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(engine);
   }, []);
->>>>>>> rakibul
 
   const particlesLoaded = useCallback(async (container) => {
     await console.log(container);

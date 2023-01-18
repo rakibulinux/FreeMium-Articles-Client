078e95b3535936dea55b107b3a26f5ec73a71967
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2023-01-18 23:06:33 +0600

    add list, stories, stats page

--- src/Pages/articlesSection/articlesDetails/ArticlesDetails.jsx
+++ src/Pages/articlesSection/articlesDetails/ArticlesDetails.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Particle from '../../../components/particleJS/Particle';
 import { useLoaderData } from 'react-router-dom';
 import ArticleDetailsCard from './articleDetailsCard/ArticleDetailsCard';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const ArticlesDetails = () => {
     const articleData= useLoaderData();
+    const {user} = useContext(AuthContext);
     return (
-        <div className='container mx-auto mt-20'>
-            <Particle />
+        <div className='container mx-auto my-20'>
+          {
+         !user?.uid && <Particle />
+        }
            <ArticleDetailsCard articleData={articleData} />
         </div>
     );

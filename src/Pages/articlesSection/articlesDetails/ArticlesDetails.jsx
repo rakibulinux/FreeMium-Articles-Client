c34fb6b1d72600c6f1cb33519961d69b0d4d4033
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-01-19 18:15:02 +0600

    article details

---
+++ src/Pages/articlesSection/articlesDetails/ArticlesDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Particle from '../../../components/particleJS/Particle';
+import { useLoaderData } from 'react-router-dom';
+import ArticleDetailsCard from './articleDetailsCard/ArticleDetailsCard';
+
+const ArticlesDetails = () => {
+    const articleData = useLoaderData();
+    return (
+        <div className='container mx-auto mt-20'>
+            <Particle />
+            <ArticleDetailsCard articleData={articleData} />
+        </div>
+    );
+};
+
+export default ArticlesDetails;

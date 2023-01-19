6cde75a0dd0508b51067584beaec179a3c76e36e
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2023-01-19 18:50:54 +0600

    solve problem

--- src/Pages/articlesSection/articlesDetails/ArticlesDetails.js
+++ src/Pages/articlesSection/articlesDetails/ArticlesDetails.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Particle from '../../../components/particleJS/Particle';
 import { useLoaderData } from 'react-router-dom';
 import ArticleDetailsCard from './articleDetailsCard/ArticleDetailsCard';
+import { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const ArticlesDetails = () => {
-    const articleData= useLoaderData();
+    const articleData = useLoaderData();
+    const { user } = useContext(AuthContext);
     return (
         <div className='container mx-auto mt-20'>
-            <Particle />
-           <ArticleDetailsCard articleData={articleData} />
+            {
+                !user?.uid && <Particle />
+            }
+            <ArticleDetailsCard articleData={articleData} />
         </div>
     );
 };

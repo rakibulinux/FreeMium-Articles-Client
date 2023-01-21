bc7b04c3e100342f9910177d39a1c9babb929be0
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2023-01-21 18:01:08 +0600

    remove footer comment

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,29 +3,28 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    // <footer className="footer p-10 hover:text-gray-100 bg-gradient-to-r from-gray-500 to-gray-600 text-white">
-    //   <div>
-    //     <span className="footer-title">Services</span>
-    //     <Link className="link link-hover">Branding</Link>
-    //     <Link className="link link-hover">Design</Link>
-    //     <Link className="link link-hover">Marketing</Link>
-    //     <Link className="link link-hover">Advertisement</Link>
-    //   </div>
-    //   <div>
-    //     <span className="footer-title">Company</span>
-    //     <Link className="link link-hover">About us</Link>
-    //     <Link className="link link-hover">Contact</Link>
-    //     <Link className="link link-hover">Jobs</Link>
-    //     <Link className="link link-hover">Press kit</Link>
-    //   </div>
-    //   <div>
-    //     <span className="footer-title">Legal</span>
-    //     <Link className="link link-hover">Terms of use</Link>
-    //     <Link className="link link-hover">Privacy policy</Link>
-    //     <Link className="link link-hover">Cookie policy</Link>
-    //   </div>
-    // </footer>
-    ''
+    <footer className="footer p-10 hover:text-gray-100 bg-gradient-to-r from-gray-500 to-gray-600 text-white">
+      <div>
+        <span className="footer-title">Services</span>
+        <Link className="link link-hover">Branding</Link>
+        <Link className="link link-hover">Design</Link>
+        <Link className="link link-hover">Marketing</Link>
+        <Link className="link link-hover">Advertisement</Link>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <Link className="link link-hover">About us</Link>
+        <Link className="link link-hover">Contact</Link>
+        <Link className="link link-hover">Jobs</Link>
+        <Link className="link link-hover">Press kit</Link>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <Link className="link link-hover">Terms of use</Link>
+        <Link className="link link-hover">Privacy policy</Link>
+        <Link className="link link-hover">Cookie policy</Link>
+      </div>
+    </footer>
   );
 };
 

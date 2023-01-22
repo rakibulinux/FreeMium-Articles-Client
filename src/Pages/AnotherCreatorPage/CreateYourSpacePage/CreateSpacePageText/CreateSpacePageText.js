9848e48878ec65e2d3e257d5cc84069b0f2a082d
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2023-01-22 10:53:22 +0600

    remove border

--- src/Pages/AnotherCreatorPage/CreateYourSpacePage/CreateSpacePageText/CreateSpacePageText.js
+++ src/Pages/AnotherCreatorPage/CreateYourSpacePage/CreateSpacePageText/CreateSpacePageText.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CreateSpacePageText = () => {
     return (
         <div className='border-[1px] lg:border-y-gray-900  lg:grid grid-cols-2 p-10 border-l-0 border-r-0 flex flex-col'>
-           <div className='lg:border-[1px] px-4 lg:border-r-gray-900 border-r-0 border-l-0 border-t-0 border-b-0'>
+           <div className='border-[1px] px-4 lg:border-r-gray-900 border-l-0 border-t-0 border-b-0'>
             <h1 className='text-4xl font-semibold text-gray-900'>Start a blog.</h1>
             <p className='mt-6 font-semibold text-gray-900'>Create a blog for free to have a personalized home for your writing. Brand your space and share your writing with readers on any device.</p>
            </div>

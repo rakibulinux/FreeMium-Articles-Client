a7b6d0d41bfb1ed971132d2bff2daa7de0646eb8
Bony <bony.075@gmail.com>
2023-01-29 22:34:14 +0600

    create dynamic tabs

--- src/Pages/UserProfileMenu/Lists/List.js
+++ src/Pages/UserProfileMenu/Lists/List.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import StaffPicks from '../../sideCategory/StaffPicks';
+import Tabs from '../Tabs/Tabs';
 
 const List = () => {
+
+    const tabsData = [
+        { id: 1, label: 'Saved', content: <div>Content for Tab 1</div> },
+        { id: 2, label: ' Highlights', content: <div>Content for Tab 2</div> },
+        
+    ];
+
+
+
+
+
     return (
         <div className='container mx-auto'>
             <div className='flex row'>
@@ -11,11 +23,26 @@ const List = () => {
                         <h1 className='text-4xl font-bold'>Your lists</h1>
                         <button className="btn btn-success bg-green-600 rounded-full  border-none text-white">New list</button>
                     </div>
-                    <div className="tabs mt-10">
+                    {/* <div className="tabs mt-10">
                         <Link className="tab tab-bordered tab-active">Saved</Link>
                         <Link className="tab tab-bordered ">Highlights</Link>
                         
-                    </div>
+                    </div> */}
+
+                    {/* tabs */}
+                    <Tabs tabsData={tabsData} />
+                    
+{/* tabs */}
+
+
+
+
+
+
+
+
+
+
                 </div>
                 <div className="divider divider-horizontal"></div>
                 <aside className="basis-1/4 px-8 hidden md:block lg:block">

cf6f50b7a35c8b8c1a4c69181304543d68f48525
Md.Al kafee <sojeebahmed.sa@gmail.com>
2023-02-01 04:46:35 +0600

    comment section design and user info added

--- src/Pages/ArticlesSection/ArticlesDetails/ArticleDetailsCard/ArticleDetailsCard.js
+++ src/Pages/ArticlesSection/ArticlesDetails/ArticleDetailsCard/ArticleDetailsCard.js
@@ -8,6 +8,7 @@ import {
 import React from "react";
 import { Link } from "react-router-dom";
 import DemoWritter from "../../../Home/DemoWritter";
+import Comments from "../../ShowMoreArtical/Comments";
 
 const ArticleDetailsCard = ({ articleData }) => {
   const {
@@ -222,9 +223,8 @@ const ArticleDetailsCard = ({ articleData }) => {
           <input type="checkbox" id="comment-modal" className="modal-toggle" />
           <div className="modal flex justify-end ">
             <div className="modal-box h-full w-full md:w-6/12 lg:w-4/12">
-              <label htmlFor="comment-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-              <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-              <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+              <label htmlFor="comment-modal" className="btn btn-sm btn-circle btn-outline border-none absolute right-2 top-2">✕</label>
+             <Comments></Comments>
             </div>
           </div>
 
---
+++ src/Pages/ArticlesSection/ShowMoreArtical/Comments.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { AuthContext } from '../../../contexts/AuthProvider';
+
+const Comments = () => {
+
+    const { user } = useContext(AuthContext);
+    // console.log(user);
+
+    const { register, handleSubmit } = useForm();
+    const handleComment = data=>{
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <h1 className='font-semibold text-2xl mb-5 '>Responses</h1>
+            <div className='flex mb-5'>
+                <img className='w-8 rounded-full' src={user?.photoURL} alt="" />
+                <h4 className='mt-1 ml-3'>{user?.displayName}</h4>
+            </div>
+
+            <form onSubmit={handleSubmit(handleComment)}>
+                <textarea className="textarea textarea-bordered textarea-sm w-full " {...register("comment")} placeholder="What are your thoughts" />
+                {/* <p>{data}</p> */}
+               <div className='flex justify-end'>
+               <input  className="btn btn-success btn-sm rounded-full text-white" type="submit" />
+               </div>
+            </form>
+
+        </div>
+    );
+};
+
+export default Comments;

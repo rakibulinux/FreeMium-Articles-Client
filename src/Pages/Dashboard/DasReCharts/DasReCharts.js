90f11f09b30b80533ac0af2ca5e89a4554c5b07f
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2023-02-05 22:34:37 +0600

    add reCharts dashbord

--- src/Pages/Dashboard/DasReCharts/DasReCharts.js
+++ src/Pages/Dashboard/DasReCharts/DasReCharts.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ProductStatusCard from './ProductStatusCard/ProductStatusCard';
+import VisitorsTraking from './VisitorsTraking/VisitorsTraking';
 
 const DasReCharts = () => {
     return (
         <div>
-            <h1>DasReCharts</h1>
+            <h1 className='text-lg font-semibold text-gray-900 underline'>FreeMium site dashboard...</h1>
+            <div className='mt-5'>
+                 <ProductStatusCard />
+                 <VisitorsTraking />
+            </div>
         </div>
     );
 };

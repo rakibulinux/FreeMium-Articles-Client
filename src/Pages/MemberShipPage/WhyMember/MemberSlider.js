dda3c9b8cbe2d7baccd4b8d694f734d76ea112e5
Rakib Linux <rakibulinux@gmail.com>
2023-02-19 05:25:34 +0600

    remove some packages

--- src/Pages/MemberShipPage/WhyMember/MemberSlider.js
+++ src/Pages/MemberShipPage/WhyMember/MemberSlider.js
@@ -1,69 +1,69 @@
-import React from 'react';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+// import React from 'react';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
-import Slider from "react-slick";
-import img1 from '../../../Assets/img1.jpg'
-import img2 from '../../../Assets/img2.jpg'
-import img3 from '../../../Assets/img3.jpg'
-const MemberSlider = () => {
+// import Slider from "react-slick";
+// import img1 from '../../../Assets/img1.jpg'
+// import img2 from '../../../Assets/img2.jpg'
+// import img3 from '../../../Assets/img3.jpg'
+// const MemberSlider = () => {
 
-  /*
-  */  
-  var settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
-        // responsive: [
-        //     {
-        //       breakpoint: 1024,
-        //       settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 3,
-        //         infinite: true,
-        //         dots: true
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 600,
-        //       settings: {
-        //         slidesToShow: 2,
-        //         slidesToScroll: 2,
-        //         initialSlide: 2
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 480,
-        //       settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //       }
-        //     }
-        //   ]
-      };
-      
-    return (
-        <div>
-             <Slider {...settings}>
-          <div>
-           <img src={img1} alt="" />
-          </div>
-          <div>
-          <img src={img2} alt="" />
-            
-          </div>
-          <div>
-          <img src={img3} alt="" />
-           
-          </div>
-          
-        </Slider>
-        </div>
-    );
-};
+//   /*
+//   */
+//   var settings = {
+//         dots: true,
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         pauseOnHover: true,
+//         // responsive: [
+//         //     {
+//         //       breakpoint: 1024,
+//         //       settings: {
+//         //         slidesToShow: 3,
+//         //         slidesToScroll: 3,
+//         //         infinite: true,
+//         //         dots: true
+//         //       }
+//         //     },
+//         //     {
+//         //       breakpoint: 600,
+//         //       settings: {
+//         //         slidesToShow: 2,
+//         //         slidesToScroll: 2,
+//         //         initialSlide: 2
+//         //       }
+//         //     },
+//         //     {
+//         //       breakpoint: 480,
+//         //       settings: {
+//         //         slidesToShow: 1,
+//         //         slidesToScroll: 1
+//         //       }
+//         //     }
+//         //   ]
+//       };
 
-export default MemberSlider;
+//     return (
+//         <div>
+//              <Slider {...settings}>
+//           <div>
+//            <img src={img1} alt="" />
+//           </div>
+//           <div>
+//           <img src={img2} alt="" />
+
+//           </div>
+//           <div>
+//           <img src={img3} alt="" />
+
+//           </div>
+
+//         </Slider>
+//         </div>
+//     );
+// };
+
+// export default MemberSlider;
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import apiReducer from "./apiSlice";
 
-// const middleware = [
-//   ...getDefaultMiddleware(),
-//   createSerializableStateInvariantMiddleware(),
-// ];
-
 const store = configureStore({
   reducer: {
     counter: counterReducer,

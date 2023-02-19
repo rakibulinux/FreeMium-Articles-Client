3bb37e51a3ee2be10e6a041519cb3bfa700336a3
Rakib Linux <rakibulinux@gmail.com>
2023-02-19 10:01:03 +0600

    added voice call

--- src/Pages/Messages/MessagesJsRightSide.js
+++ src/Pages/Messages/MessagesJsRightSide.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { format } from "timeago.js";
+import VoiceCall from "./VoiceCall/VoiceCall";
 const MessagesJsRightSide = ({
   currentFriend,
   friends,
@@ -15,7 +16,13 @@ const MessagesJsRightSide = ({
 }) => {
   const { picture, name } = currentFriend;
 
-  const emojis = [ "😨", "🤔", "😕", "😬","😊","😳",
+  const emojis = [
+    "😨",
+    "🤔",
+    "😕",
+    "😬",
+    "😊",
+    "😳",
     "😲",
     "😟",
     "😒",
@@ -55,79 +62,80 @@ const MessagesJsRightSide = ({
             <div className="font-semibold text-sm">{name}</div>
             <div className="text-xs text-gray-500">Active</div>
           </div>
-          {/* <div className="ml-auto">
-              <ul className="flex flex-row items-center space-x-2">
-                <li>
-                  <Link
-                    to="/"
-                    className="flex items-center justify-center bg-gray-100 hover:bg-gray-200 text-gray-400 h-10 w-10 rounded-full"
-                  >
-                    <span>
-                      <svg
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        stroke="none"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                        ></path>
-                      </svg>
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/"
-                    className="flex items-center justify-center bg-gray-100 hover:bg-gray-200 text-gray-400 h-10 w-10 rounded-full"
-                  >
-                    <span>
-                      <svg
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        stroke="none"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-                        ></path>
-                      </svg>
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/"
-                    className="flex items-center justify-center bg-gray-100 hover:bg-gray-200 text-gray-400 h-10 w-10 rounded-full"
-                  >
-                    <span>
-                      <svg
-                        className="w-5 h-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-                        ></path>
-                      </svg>
-                    </span>
-                  </Link>
-                </li>
-              </ul>
-            </div> */}
+          <div className="ml-auto">
+            <ul className="flex flex-row items-center space-x-2">
+              {/* <li>
+                <Link
+                  to="/"
+                  className="flex items-center justify-center bg-gray-100 hover:bg-gray-200 text-gray-400 h-10 w-10 rounded-full"
+                >
+                  <span>
+                    <svg
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      stroke="none"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                      ></path>
+                    </svg>
+                  </span>
+                </Link>
+              </li> */}
+              <VoiceCall />
+              <li>
+                <Link
+                  to="/"
+                  className="flex items-center justify-center bg-gray-100 hover:bg-gray-200 text-gray-400 h-10 w-10 rounded-full"
+                >
+                  <span>
+                    <svg
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      stroke="none"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+                      ></path>
+                    </svg>
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="flex items-center justify-center bg-gray-100 hover:bg-gray-200 text-gray-400 h-10 w-10 rounded-full"
+                >
+                  <span>
+                    <svg
+                      className="w-5 h-5"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                      ></path>
+                    </svg>
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="h-full overflow-hidden py-4">
           <div className="h-full overflow-y-auto">
@@ -159,8 +167,17 @@ const MessagesJsRightSide = ({
                           >
                             me
                           </div>
-                          <div key={m?._id + 5} className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                            <div key={m?._id + 5}>{m?.message?.text===""?<img src={m?.message?.image} alt="img"></img>: m?.message?.text}</div>
+                          <div
+                            key={m?._id + 5}
+                            className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                          >
+                            <div key={m?._id + 5}>
+                              {m?.message?.text === "" ? (
+                                <img src={m?.message?.image} alt="img"></img>
+                              ) : (
+                                m?.message?.text
+                              )}
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -173,7 +190,13 @@ const MessagesJsRightSide = ({
                             className="flex items-center justify-center h-10 w-10 rounded-full  flex-shrink-0"
                           ></img>
                           <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                          <div>{m?.message?.text===""?<img src={m?.message?.image} alt="img"></img>: m?.message?.text}</div>
+                            <div>
+                              {m?.message?.text === "" ? (
+                                <img src={m?.message?.image} alt="img"></img>
+                              ) : (
+                                m?.message?.text
+                              )}
+                            </div>
                           </div>
                         </div>
                       </div>

75a08cb21801e4ffa9e09117ad62ac1b2ca9fed4
Rakib Linux <rakibulinux@gmail.com>
2023-02-26 21:20:04 +0600

    get new code

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -11,12 +11,12 @@ const Main = () => {
       <div
         className={
           isDarkMode
-            ? "w-11/12 md:w-11/12 lg:w-11/12 xl:w-10/12 mx-auto bg-black-350 text-white"
-            : "w-11/12 md:w-11/12 lg:w-11/12 xl:w-10/12 mx-auto bg-base-100 text-black-350"
+            ? "w-11/12 mx-auto bg-black-350 text-white"
+            : "w-11/12 mx-auto bg-base-100 text-black-350"
         }
-      ></div>
-
-      <Outlet />
+      >
+        <Outlet />
+      </div>
     </div>
   );
 };
